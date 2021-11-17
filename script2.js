8d281164831a4315535087d89b255f54b08daf90
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-11-17 20:50:59 +0900

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,45 @@
+// function sayHello() {
+//     console.log("Hello");
+// }
+// sayHello();
+// sayHello("nicolas");    //Hello 출력 
+
+// function sayHello(nameOfPerson, weight) {
+//     console.log("Hello my name is " + nameOfPerson + ", and I'm " + weight + "kg.");
+// }
+// sayHello("nicolas", 65);
+// sayHello("chanwoo", 60);
+// sayHello("shintaeil", 120);
+
+// function plus(a ,b) {
+//     console.log(a + b);
+// }
+// function divide(a, b) {
+//     console.log(a / b);
+// }
+
+// plus(8, 7);
+// divide(15, 3);
+
+// const player = {
+//     name: "nico",
+//     sayHello: function (otherPersonsName) {
+//         console.log("hello " + otherPersonsName + " nice to meet you");
+//     },
+// };
+
+// console.log(player.name);
+// player.sayHello("tail");
+
+// const a = 5;
+// let isNicoFat = true;
+// isNicoFat = false;
+
+const me = "cool";
+const days = [1, 2, 123, "shintail", me];
+console.log(days);
+console.log(days[3]);
+days[3] = "tail's daesinman";
+console.log(days[3]);
+alert(days[3]);
+days.push("ssimryun");  //days[] 배열 맨 뒤에 추가.
