9e6fd055350bf914a72b899784ea93234e5c31f6
선채훈 <80810317+sunchaehoon@users.noreply.github.com>
2021-12-19 16:24:16 +0900

    Create weatherF.js

---
+++ weatherF.js
@@ -0,0 +1,21 @@
+const API_KEY = "eab77d50a9b3acda4f6636f84881c1ef";
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    console.log("You live in", lat, lon);
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
